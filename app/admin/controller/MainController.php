d86ec4fa201dba633f5197af066c401f0a2afd2a
北岸的云 <hiwower@gmail.com>
2017-11-27 16:08:55 +0800

    后台功能进行删减

--- app/admin/controller/MainController.php
+++ app/admin/controller/MainController.php
@@ -78,7 +78,7 @@ class MainController extends AdminBaseController
 
         $this->assign('dashboard_widgets', $dashboardWidgets);
         $this->assign('dashboard_widget_plugins', $dashboardWidgetPlugins);
-        $this->assign('has_smtp_setting', empty($smtpSetting) ? false : true);
+        $this->assign('has_smtp_setting',  true);
 
         return $this->fetch();
     }
